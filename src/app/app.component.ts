2bceccc97003111cd29ce450d4a5ce3b9bfad4a7
Daniel Felipe Florez Salgado <danielflorez111@hotmail.com>
2017-11-29 19:59:59 -0500

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  todoList = [
+    {task:'Task 1', done:false}, 
+    {task:'Task 2', done:false} 
+  ];
+
+  task:string;
+  status:string;
+
+  constructor() { }
+
+  addTask() {
+    this.todoList.push({ task: this.task,  done: false });
+    this.task = "";
+  };
+
+  deleteTask(i) {
+    this.todoList.splice(i, 1);
+  }
+
 }
